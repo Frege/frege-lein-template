ffb0f2dd77dd2662007a377784423ec6b638a6d7
zjhmale <zjhmale@gmail.com>
2015-10-03 02:08:58 +0800

    update lein-fregec version

--- src/leiningen/new/frege/project.clj
+++ src/leiningen/new/frege/project.clj
@@ -11,7 +11,7 @@
                  [org.clojure/clojure "1.6.0"]
                  ;; need to depend on Frege for runtime:
                  [com.theoryinpractise.frege/frege "3.22.367-g2737683"]]
-  :plugins [[lein-fregec "3.22.367-i"]]
+  :plugins [[lein-fregec "3.22.524"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   :frege-source-paths ["src/frege"]
