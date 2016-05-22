6c7f9002dc4ecfe9455217de1e7fea998f01166d
Sean Corfield <sean@corfield.org>
2016-05-22 14:23:26 -0700

    Add lazy calls and eager fib

--- src/leiningen/new/frege/core.clj
+++ src/leiningen/new/frege/core.clj
@@ -8,4 +8,10 @@
   [& args]
   (println "Hello, World!")
   (doseq [n (range 10)]
-    (println "Fibonacci number" n "is" (Fibonacci/fib n))))
+    (println "Fibonacci number" n "is"
+             ;; Java 7 compatible Frege; pass lazy long
+             (Fibonacci/fib (frege.run7.Thunk/lazy n))
+             "(called via lazy fib)")
+    (println "Fibonacci number" n "is"
+             (Fibonacci/fib$tick n)
+             "(called via eager fib')")))
--- src/leiningen/new/frege/core_test.clj
+++ src/leiningen/new/frege/core_test.clj
@@ -5,7 +5,13 @@
   (:import Fibonacci))
 
 (deftest test-fib
-  (is (= 0 (Fibonacci/fib 0)))
-  (is (= 1 (Fibonacci/fib 1)))
-  (is (= 1 (Fibonacci/fib 2)))
-  (is (= 55 (Fibonacci/fib 10))))
+  (is (= 0 (Fibonacci/fib (frege.run7.Thunk/lazy 0))))
+  (is (= 1 (Fibonacci/fib (frege.run7.Thunk/lazy 1))))
+  (is (= 1 (Fibonacci/fib (frege.run7.Thunk/lazy 2))))
+  (is (= 55 (Fibonacci/fib (frege.run7.Thunk/lazy 10)))))
+
+(deftest test-fib'
+  (is (= 0 (Fibonacci/fib$tick 0)))
+  (is (= 1 (Fibonacci/fib$tick 1)))
+  (is (= 1 (Fibonacci/fib$tick 2)))
+  (is (= 55 (Fibonacci/fib$tick 10))))
